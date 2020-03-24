aef42be5899480fc23695db5bb8c4aca047fd74f
DreamingRaven <GeorgeRavenNR@outlook.com>
2020-03-24 14:06:21 +0000

    Added unit tests for fhe and magic functions

--- fhe.py
+++ fhe.py
@@ -7,7 +7,8 @@
 # @License: please see LICENSE file in project root
 
 import os
-import seal  # github.com/Huelse/SEAL-Python or DreamingRaven/seal-python
+import unittest
+# import seal  # github.com/Huelse/SEAL-Python or DreamingRaven/seal-python
 import numpy as np
 import pickle
 
@@ -40,11 +41,10 @@ class Fhe(object):
             "pylog": logger if logger is not None else print,
 
         }
-        self.args = self._mergeDicts(defaults, args)
+        self.args = self._merge_dictionary(defaults, args)
         # final adjustments to newly defined dictionary
-        pass
 
-    def _mergeDicts(self, *dicts):
+    def _merge_dictionary(self, *dicts):
         """Given multiple dictionaries, merge together in order."""
         result = {}
         for dictionary in dicts:
@@ -55,7 +55,57 @@ class Fhe(object):
         """Display current internal state."""
         self.args["pylog"](self.args)
 
+    def __setitem__(self, key, value):
+        """Set a single arg or state by, (key, value)."""
+        self.args[key] = value
+
+    __setitem__.__annotations__ = {"key": str, "value": any, "return": None}
+
+    def __getitem__(self, key):
+        """Get a single arg or state by, (key, value)."""
+        try:
+            return self.args[key]
+        except KeyError:
+            return None  # does not exist is the same as None, gracefull catch
+
+    __getitem__.__annotations__ = {"key": str, "return": any}
+
+    def __delitem__(self, key):
+        """Delete a single arg or state by, (key, value)."""
+        try:
+            del self.args[key]
+        except KeyError:
+            pass  # job is not done but equivalent outcomes so will not error
+
+    __delitem__.__annotations__ = {"key": str, "return": None}
+
+
+class Fhe_tests(unittest.TestCase):
+
+    def test_init(self):
+        """Test fhe object initialisation, by using magic function"""
+        self.assertEqual(Fhe({"fhe_data": [30]})["fhe_data"], [30])
+
+    def test_magic_get(self):
+        obj = Fhe({"test": 30})
+        self.assertEqual(obj["test"], 30)
+
+    def test_magic_set(self):
+        obj = Fhe({"test": 30})
+        obj["test"] = 40
+        self.assertEqual(obj["test"], 40)
+
+    def test_magic_del(self):
+        obj = Fhe({"test": 30})
+        del obj["test"]
+        self.assertEqual(obj["test"], None)
+
+    def test_merge_dictionary(self):
+        self.assertEqual(Fhe()._merge_dictionary({}, {}), {})
+        self.assertEqual(Fhe()._merge_dictionary({"x": 1, "y": 1},
+                                                 {"x": 2}), {"x": 2, "y": 1})
+
 
 if __name__ == "__main__":
-    fhe = Fhe({"fhe_data": None})
-    fhe.debug()
+    # run all the unit-tests
+    unittest.main()
