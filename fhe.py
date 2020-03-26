731525d4a7700a7dba48bc805b8328a798338c88
DreamingRaven <GeorgeRavenNR@outlook.com>
2020-03-26 13:27:54 +0000

    Added parameter printing, key generation, context
    key generation still under completion but core is there.

--- fhe.py
+++ fhe.py
@@ -47,6 +47,8 @@ class Fhe(object):
             "pylog": logger if logger is not None else print,
             "fhe_scheme_type": seal.scheme_type.CKKS,
             "fhe_poly_modulus_degree": 8192,
+            "fhe_coeff_modulus": [60, 40, 40, 60],
+            "fhe_context": None,
         }
         self.args = self._merge_dictionary(defaults, args)
         # final adjustments to newly defined dictionary
@@ -70,34 +72,112 @@ class Fhe(object):
     _merge_dictionary.__annotations__ = {"*dicts": dict, "return": dict}
 
     def create_context(self, fhe_scheme_type=None,
-                       fhe_poly_modulus_degree=None):
+                       fhe_poly_modulus_degree=None,
+                       fhe_coeff_modulus=None):
         """Create an encryption context for encrypting and decrypting data,
         according to an implementation shceme.
 
         :param fhe_scheme_type: seal.scheme_type to use for context.
         :type fhe_scheme_type: seal.scheme_type
+        :param fhe_poly_modulus_degree: .
+        :type fhe_poly_modulus_degree: seal.scheme_type
+        :param fhe_scheme_type: seal.scheme_type to use for context.
+        :type fhe_scheme_type: seal.scheme_type
         :return: Seal context to use for encryption.
         :rtype: seal.SEALContext
         """
+        # manage all inputs
         scheme = fhe_scheme_type if fhe_scheme_type is not None \
             else self.args["fhe_scheme_type"]
         poly_mod_deg = fhe_poly_modulus_degree if fhe_poly_modulus_degree is \
             not None else self.args["fhe_poly_modulus_degree"]
-        context = None
+        coeff_mod = fhe_coeff_modulus if fhe_coeff_modulus is not None else \
+            self.args["fhe_coeff_modulus"]
 
-        # self.args["pylog"](help(seal.CoeffModulus))
         self.args["pylog"](seal.CoeffModulus.MaxBitCount(poly_mod_deg),
                            "is the max number of bits we can use in the poly",
                            "modulus degree")
+        # check if we exceed the maximum number of bits in coefficient modulus
+        max_bit_count = seal.CoeffModulus.MaxBitCount(poly_mod_deg)
+        bit_count = np.array(self.args["fhe_coeff_modulus"]).sum()
+        if(bit_count <= max_bit_count):
 
-        params = seal.EncryptionParameters(scheme)
-        params.set_poly_modulus_degree(poly_mod_deg)
-        # params.set_coeff_modulus(seal.CoeffModulus.Create())
-        self.args["fhe_context"] = context
-        return context
+            params = seal.EncryptionParameters(scheme)
+            params.set_poly_modulus_degree(poly_mod_deg)
+            params.set_coeff_modulus(
+                seal.CoeffModulus.Create(poly_mod_deg,
+                                         self.args["fhe_coeff_modulus"]))
+
+            context = seal.SEALContext.Create(params)
+            self.args["fhe_context"] = context
+            self.log_parameters(context)
+            return context
+        else:
+            self.args["pylog"](self.args["fhe_coeff_modulus"],
+                               "exceeds the maximum number of bits for a",
+                               "poly_modulus_degree of:",
+                               poly_mod_deg, "which is a cumulative total of:",
+                               max_bit_count)
+            return None
 
     create_context.__annotations__ = {"fhe_scheme_type": seal.scheme_type,
                                       "return": seal.SEALContext}
+
+    def log_parameters(self, fhe_context=None):
+        """Log encryption parameters by context.
+
+        Temporary original version from github huelse/seal-python example
+        :param fhe_context: Seal context used to manipulate data.
+        :type fhe_context: seal.SEALContext
+        :return: None.
+        :rtype: None
+        """
+        context = fhe_context if fhe_context is not None else \
+            self.args["fhe_context"]
+        context_data = context.key_context_data()
+        if context_data.parms().scheme() == seal.scheme_type.BFV:
+            scheme_name = "BFV"
+        elif context_data.parms().scheme() == seal.scheme_type.CKKS:
+            scheme_name = "CKKS"
+        else:
+            scheme_name = "unsupported scheme"
+        print("/")
+        print("| Encryption parameters:")
+        print("| scheme: " + scheme_name)
+        print("| poly_modulus_degree: " +
+              str(context_data.parms().poly_modulus_degree()))
+        print("| coeff_modulus size: ", end="")
+        coeff_modulus = context_data.parms().coeff_modulus()
+        coeff_modulus_sum = 0
+        for j in coeff_modulus:
+            coeff_modulus_sum += j.bit_count()
+        print(str(coeff_modulus_sum) + "(", end="")
+        for i in range(len(coeff_modulus) - 1):
+            print(str(coeff_modulus[i].bit_count()) + " + ", end="")
+        print(str(coeff_modulus[-1].bit_count()) + ") bits")
+        if context_data.parms().scheme() == seal.scheme_type.BFV:
+            print("| plain_modulus: " +
+                  str(context_data.parms().plain_modulus().value()))
+        print("\\")
+
+    log_parameters.__annotations__ = {"fhe_context": seal.SEALContext,
+                                      "return": None}
+
+    def generate_keys(self, fhe_context=None):
+        """Generate public, private, and relin keys.
+
+        Given encryption context keys will be stored in internal dictionary,
+        and returned as a seperate dictionary.
+        """
+        context = fhe_context if fhe_context is not None else \
+            self.args["fhe_context"]
+        keygen = seal.KeyGenerator(context)
+        key_dict = {
+            "fhe_public_key": keygen.public_key(),
+            "fhe_secret_key": keygen.secret_key(),
+            "fhe_relin_key": keygen.relin_keys(),
+        }
+        return key_dict
 
     def debug(self):
         """Display current internal state of all values.
