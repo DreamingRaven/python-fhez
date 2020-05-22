b6fdc8fe6bce87ff4c06e60462e3831859022bed
DreamingRaven <GeorgeRavenNR@outlook.com>
2020-05-22 12:50:19 +0100

    Added new evector arithmetic processor and handler
    This class will handle all new vectors/ arrays so computing on them
    becomes a lot easier and calls the underlying microsoft seal correctly.

---
+++ fhe/evector.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+# @Author: GeorgeRaven <archer>
+# @Date:   2020-05-22T10:23:21+01:00
+# @Last modified by:   archer
+# @Last modified time: 2020-05-22T12:45:10+01:00
+# @License: please see LICENSE file in project root
+
+from fhe import Fhe
+import unittest
+
+
+class Evector(object):
+    """Encrypted vector arithmetic and processor class."""
+
+    def __init__(self, args=None, logger=None):
+        pass
+
+
+class Evector_tests(unittest.TestCase):
+    """Unit test class aggregating all tests for the encrypted vector class."""
+
+
+if __name__ == "__main__":
+    # run all the unit-tests
+    print("now testing:", __file__, "...")
+    unittest.main()
