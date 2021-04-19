9ac40b6630aa0fb8507cb9a363aac8058a6ec39b
GeorgeRaven <GeorgeRavenCommunity@pm.me>
2021-04-19 21:44:45 +0100

    Renaming graph to net and added networkx depend

--- fhe/nn/graph/graph.py
+++ fhe/nn/graph/graph.py
@@ -3,13 +3,14 @@
 # @Author: GeorgeRaven <archer>
 # @Date:   2021-04-15T14:24:29+01:00
 # @Last modified by:   archer
-# @Last modified time: 2021-04-16T16:15:38+01:00
+# @Last modified time: 2021-04-19T21:44:08+01:00
 # @License: please see LICENSE file in project root
 
+import unittest
 from fhe.nn.graph.node import Node
 
 
-class Graph(object):
+class Net(object):
     """Graph representing neural network computations as a network of nodes."""
 
     def __init__(self):
@@ -45,3 +46,7 @@ class Graph(object):
     def traverse(self):
         """Traverse and yield nodes on the graph, depth first."""
         pass
+
+
+class net_tests(unittest.TestCase):
+    """Testing net class."""
