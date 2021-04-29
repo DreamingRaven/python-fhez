4cfe385d084d973a99624f3b1f232c6c31bb2189
GeorgeRaven <GeorgeRavenCommunity@pm.me>
2021-04-29 11:50:37 +0100

    Re-arranged nodes + edges
    The order of the nodes + edges now properly represents the processing
    order so when graphed they gradient in colour and size properly.

--- fhe/nn/graph/net.py
+++ fhe/nn/graph/net.py
@@ -3,7 +3,7 @@
 # @Author: GeorgeRaven <archer>
 # @Date:   2021-04-15T14:24:29+01:00
 # @Last modified by:   archer
-# @Last modified time: 2021-04-26T12:04:16+01:00
+# @Last modified time: 2021-04-29T11:49:24+01:00
 # @License: please see LICENSE file in project root
 
 import os
@@ -130,23 +130,23 @@ class net_tests(unittest.TestCase):
         net = Net()
         # populate basic graph
         graph.add_node("x")
-        graph.add_node("y")
-        graph.add_node("sum")
-        graph.add_node("loss")
         graph.add_node("cnn-0",
                        type="neuron",
                        nn=Layer_CNN(weights=(1, 3, 3, 3),
                                     stride=[1, 3, 3, 3],
                                     bias=0))
+        graph.add_edge("x", "cnn-0")
         graph.add_node("ann-0",
                        type="neuron",
                        nn=Layer_ANN(weights=(5,), bias=0))
-
-        graph.add_edge("x", "cnn-0")
         graph.add_edge("cnn-0", "ann-0")
+        graph.add_node("sum")
         graph.add_edge("ann-0", "sum")
+        graph.add_node("loss")
         graph.add_edge("sum", "loss")
+        graph.add_node("y")
         graph.add_edge("y", "loss")
+
         # return basic graph in net
         net.graph = graph
         return net
