5bb52e98117eb5909b283413399a2d0a95fe6a93
GeorgeRaven <GeorgeRavenCommunity@pm.me>
2021-07-24 15:37:09 +0100

    Split ann tests up just like graph

--- fhez/nn/layer/ann.py
+++ fhez/nn/layer/ann.py
@@ -3,16 +3,10 @@
 # @Author: GeorgeRaven <archer>
 # @Date:   2020-09-16T11:33:51+01:00
 # @Last modified by:   archer
-# @Last modified time: 2021-07-23T16:16:50+01:00
+# @Last modified time: 2021-07-24T15:34:25+01:00
 # @License: please see LICENSE file in project root
 
-import unittest
-import logging as logger
 import numpy as np
-
-from tqdm import tqdm
-from fhez.rearray import ReArray
-from fhez.nn.layer.layer import Layer
 from fhez.nn.graph.node import Node
 
 
@@ -80,86 +74,3 @@ class ANN(Node):
     def cost(self):
         """Get no cost of a this node."""
         return 2
-
-
-class Ann_Tests(unittest.TestCase):
-
-    @property
-    def data_shape(self):
-        return (3, 32, 32, 3)
-
-    @property
-    def data(self):
-        """Get some generated data."""
-        # array = np.arange(1*32*32*3)
-        # array.shape = (1, 32, 32, 3)
-        array = np.random.rand(*self.data_shape)
-        return array
-
-    @property
-    def reseal_args(self):
-        """Get some reseal arguments for encryption."""
-        return {
-            "scheme": seal.scheme_type.CKKS,
-            "poly_modulus_degree": 8192*2,  # 438
-            # "coefficient_modulus": [60, 40, 40, 60],
-            "coefficient_modulus":
-                [45, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 45],
-            "scale": pow(2.0, 30),
-            "cache": True,
-        }
-
-    def setUp(self):
-        """Start timer and init variables."""
-        import time
-
-        self.weights = (3,)  # if tuple allows cnn to initialise itself
-        self.stride = [1, 3, 3, 3]  # stride list per-dimension
-        self.bias = 0  # assume no bias at first
-
-        self.startTime = time.time()
-
-    def tearDown(self):
-        """Calculate and print time delta."""
-        import time  # dont want time to be imported unless testing as unused
-        t = time.time() - self.startTime
-        print('%s: %.3f' % (self.id(), t))
-
-    def test_test(self):
-        """Check our testing values meet requirements."""
-        # check data is the shape we desire/ gave it to generate
-        self.assertEqual(self.data.shape, self.data_shape)
-        # check weights length matches first dim of data
-        self.assertEqual(len(self.weights), self.data_shape[0])
-        # check data is between 0-1
-        self.assertLessEqual(self.data[0], 1)
-
-    def test_init(self):
-        """Check object initialisation works."""
-        ANN(weights=self.weights, bias=self.bias)
-
-    def test_forward(self, data=None):
-        """Check forward pass works as expected."""
-        ann = ANN(weights=self.weights, bias=self.bias)
-        data = self.data if data is None else data
-        acti = ann.forward(x=data)
-        return acti
-
-    def test_forward_enc(self):
-        """Check encrypted forward pass works as expected."""
-        data = ReArray(self.data, **self.reseal_args)
-        self.assertIsInstance(data, ReArray)
-        acti = self.test_forward(data=data)
-        self.assertIsInstance(acti, ReArray)
-        plain = np.array(acti)
-        self.assertIsInstance(plain, np.ndarray)
-
-
-if __name__ == "__main__":
-    logger.basicConfig(  # filename="{}.log".format(__file__),
-        level=logger.INFO,
-        format="%(asctime)s %(levelname)s:%(message)s",
-        datefmt="%Y-%m-%dT%H:%M:%S")
-    # run all the unit-tests
-    print("now testing:", __file__, "...")
-    unittest.main()
