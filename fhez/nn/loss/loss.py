2d89eac3861329e2d7ed39a4b1026529eb328377
GeorgeRaven <GeorgeRavenCommunity@pm.me>
2021-07-29 10:43:58 +0100

    Added MSE

--- fhez/nn/loss/loss.py
+++ fhez/nn/loss/loss.py
@@ -3,7 +3,7 @@
 # @Author: George Onoufriou <archer>
 # @Date:   2021-07-28T21:37:24+01:00
 # @Last modified by:   archer
-# @Last modified time: 2021-07-28T23:01:11+01:00
+# @Last modified time: 2021-07-29T09:57:46+01:00
 
 import numpy as np
 
@@ -22,3 +22,11 @@ def mse(y: np.array, y_hat: np.array):
     :math:`\text{MSE}=\frac{\sum_{i=0}^{N-1} (y-\hat{y})^2 }{N}`
     """
     return np.mean(np.square(y - y_hat))
+
+
+def rmse(y: np.array, y_hat: np.array):
+    r"""Calculate the Mean of the Squared Error (MSE).
+
+    :math:`\text{RMSE}=\sqrt{\frac{\sum_{i=0}^{N-1} (y-\hat{y})^2 }{N}}`
+    """
+    return np.sqrt(np.mean(np.square(y - y_hat)))
