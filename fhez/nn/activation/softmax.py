Removed defunct print and fixed softmax test
Softmax is working optimally now, I have confirmed the results using
known results for softmax, despite having a mildly convoluted way of
calculating the backward pass since it has this awkward condition.

fhez/nn/activation/softmax.py
@@ -2,7 +2,7 @@
 # @Author: George Onoufriou <archer>
 # @Date:   2021-08-02T22:00:06+01:00
 # @Last modified by:   archer
-# @Last modified time: 2021-08-09T15:32:24+01:00
+# @Last modified time: 2021-08-09T15:50:12+01:00
 
 import numpy as np
 from fhez.nn.graph.node import Node
@@ -39,7 +39,7 @@ class Softmax(Node):
         classification.
         """
         # softmax derivative does not need x it needs the x_softmaxed
-        x = np.array(self.inputs.pop())  # note this is x_softmaxed
+        x = np.array(self.inputs.pop())  # note this is softmaxed ie py_hat
         # calculate class specific gradient
         dfdx = (x * (1-x)) * gradient
         # calculate inter class gradient
@@ -47,7 +47,7 @@ class Softmax(Node):
             t = -x[i] * x * gradient[i]
             t[i] = 0  # already calc class specific grad differently above
             dfdx += t
-        return dfdx
+        return dfdx  # gradient with respect to all inputs
 
     @property
     def cost(self):

fhez/nn/activation/softmax_test.py
@@ -1,7 +1,7 @@
 # @Author: George Onoufriou <archer>
 # @Date:   2021-08-02T22:01:04+01:00
 # @Last modified by:   archer
-# @Last modified time: 2021-08-09T15:24:27+01:00
+# @Last modified time: 2021-08-09T16:04:55+01:00
 
 import time
 import unittest
@@ -29,16 +29,24 @@ class SoftmaxTest(unittest.TestCase):
         self.assertIsInstance(activation, Softmax)
 
     def test_backward(self):
-        a = [1.0, 2.0, 3.0]
+        """Test softmax backward pass with known values."""
+        a = [1.42, -0.4, 0.23]
         softmax = Softmax()
         py_hat = softmax.forward(a)
-        truth = [0.024, 0.064, 0.175]
-        softmax.backward(gradient=np.array([0, 0, 0, 1, 0, 0, 0]))
+        py_hat_truth = [0.682, 0.111, 0.207]
+        np.testing.assert_array_almost_equal(py_hat, py_hat_truth,
+                                             decimal=3, verbose=True)
+        grad = softmax.backward(gradient=np.array([1, 0, 0]))
+        grad_truth = np.array([0.217, -0.075, -0.142])
+        print("SOFTMAX GRADIENT", grad)
+        np.testing.assert_array_almost_equal(grad, grad_truth,
+                                             decimal=3, verbose=True)
 
     def test_forward(self):
-        x = [1.0, 2.0, 3.0, 4.0, 1.0, 2.0, 3.0]
+        """Test softmax forward pass with known values."""
+        a = [1.42, -0.4, 0.23]
         softmax = Softmax()
-        a = softmax.forward(x)
-        truth = [0.024, 0.064, 0.175, 0.475, 0.024, 0.064, 0.175]
-        np.testing.assert_array_almost_equal(a, truth,
+        py_hat = softmax.forward(a)
+        py_hat_truth = [0.682, 0.111, 0.207]
+        np.testing.assert_array_almost_equal(py_hat, py_hat_truth,
                                              decimal=3, verbose=True)

fhez/nn/loss/cce_test.py
@@ -2,7 +2,7 @@
 # @Author: George Onoufriou <archer>
 # @Date:   2021-08-02T22:04:55+01:00
 # @Last modified by:   archer
-# @Last modified time: 2021-08-09T15:35:48+01:00
+# @Last modified time: 2021-08-09T15:45:26+01:00
 
 import time
 import unittest
@@ -64,7 +64,6 @@ class CrossEntropyTest(unittest.TestCase):
         np.testing.assert_array_almost_equal(loss, loss_true,
                                              decimal=3,
                                              verbose=True)
-        print("CC LOSS:", loss)
 
     def test_forward_exact(self):
         """Check perfect CCE forward pass is 0."""