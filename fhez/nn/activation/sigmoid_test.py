32d11f7b7a3db3cac2436372ee8cd60dc3406871
GeorgeRaven <GeorgeRavenCommunity@pm.me>
2021-08-09 17:20:33 +0100

    Fully convertetd sigmoid approximation

--- fhez/nn/activation/sigmoid_test.py
+++ fhez/nn/activation/sigmoid_test.py
@@ -1,7 +1,7 @@
 # @Author: George Onoufriou <archer>
 # @Date:   2021-08-09T16:48:31+01:00
 # @Last modified by:   archer
-# @Last modified time: 2021-08-09T17:02:24+01:00
+# @Last modified time: 2021-08-09T17:20:21+01:00
 
 import time
 import unittest
@@ -23,26 +23,24 @@ class Sigmoid_Test(unittest.TestCase):
 
     @property
     def x(self):
-        x = [
-            [0.0, 0.2, 0.3],
-            [0.8, 0.9, 1.0]
-        ]
+        x = [1, -1, 0.7]
         return np.array(x)
 
     def test_forward(self):
+        """Check sigmoid forward pass against known truth, plaintext."""
         print(self.x.shape)
         activation_function = Sigmoid()
         activation = activation_function.forward(self.x)
-        truth = activation_function.sigmoid(self.x)
-        print(activation)
-        print(truth)
+        # activation_truth = activation_function.sigmoid(self.x)
+        activation_truth = np.array([0.693, 0.307, 0.637])
+        np.testing.assert_array_almost_equal(activation, activation_truth,
+                                             decimal=3, verbose=True)
 
     def test_backward(self):
+        """Check sigmoid backward pass against known truth, plaintext."""
         activation_function = Sigmoid()
-        activation = activation_function.forward(self.x)
+        activation_function.forward(self.x)
         df_dx = activation_function.backward(np.array([1]))
-        print(activation.shape)
-        print(df_dx.shape)
-
-    def test_sigmoid(self):
-        raise NotImplementedError("Sigmoid implementation is incomplete.")
+        df_dx_truth = np.array([0.185, 0.185, 0.1911])
+        np.testing.assert_array_almost_equal(df_dx, df_dx_truth,
+                                             decimal=3, verbose=True)
