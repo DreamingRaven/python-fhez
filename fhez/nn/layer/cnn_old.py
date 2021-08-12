3affb98188fa8ca530240fb657c2cf5eae93ea00
GeorgeRaven <GeorgeRavenCommunity@pm.me>
2021-08-12 16:47:42 +0100

    Added newstyle CNN outline and tests
    Removed old CNN, carried over some of the better/ usefull utilities, and
    I have outlined the basic class abstraction. Now to populate it, and to
    complete all tests.

--- fhez/nn/layer/cnn_old.py
+++ fhez/nn/layer/cnn_old.py
@@ -1,125 +1,111 @@
-"""Convolutional Neural Network (CNN) as node abstraction."""
+#!/usr/bin/env python3
+
 # @Author: GeorgeRaven <archer>
 # @Date:   2020-09-16T11:33:51+01:00
 # @Last modified by:   archer
-# @Last modified time: 2021-08-12T16:46:40+01:00
+# @Last modified time: 2021-08-11T15:47:59+01:00
 # @License: please see LICENSE file in project root
 
-import copy
+import logging as logger
 import numpy as np
-from fhez.nn.graph.node import Node
+import unittest
+import copy
+
+from tqdm import tqdm
+
+import seal
+from fhez.rearray import ReArray
+from fhez.nn.layer.layer import Layer
 
 
-class CNN(Node):
-    """Convolutional Neural Network."""
+class Layer_CNN(Layer):
 
-    def __init__(self,
-                 weights: np.ndarray = None,
-                 stride: np.ndarray = None,
-                 bias: np.ndarray = None,
-                 optimiser=None):
-        """Create CNN object with any desired default parameters."""
-        if weights is not None:
-            self.weights = weights
-        if stride is not None:
-            self.stride = stride
-        if bias is not None:
-            self.bias = bias
-        if optimiser is not None:
-            self.optimiser = optimiser
+    @Layer.fwd
+    def forward(self, x: (np.array, ReArray)):
+        """Take lst of batches of x, return activated output lst of layer."""
 
-    @property
-    def cost(self):
-        """Get computational cost of this CNN node."""
-        return 6
+        # stride over x using our convolutional filter
+        # lets say x = (64, 32, 32, 3) or x_1D = (3, 100, 1, 8)
+        # also could be x = (64, 32, 32, crypt), x = (3, 100, 1, crypt)
+        # and filter is (32, 3, 3, 3) or x_1D = (1, 100, 1, 8)
 
-    def forward(self, x: np.ndarray):
-        """Compute convolutional filter forward pass and sums."""
-        self.inputs.append(x)
+        # if no cross correlation windows have been specified create them
+        # and cache them for later re-use as uneccessary to re-compute
         if self.windows is None:
             self.windows = self.windex(data=x.shape[1:],
                                        filter=self.weights.shape[1:],
                                        stride=self.stride[1:])
             self.windows = list(map(self.windex_to_slice, self.windows))
-        raise NotImplementedError("CNN forward not yet implemented.")
 
-    def backward(self, gradient: np.ndarray):
-        """Compute computational filter gradient and input gradient."""
-        raise NotImplementedError("CNN backward not yet implemented.")
+        activated = []
+        # apply each window and do it by index so can state progress
+        for i in tqdm(range(len(self.windows)), desc="{}.{}".format(
+                self.__class__.__name__, "forward"),
+            ncols=80, colour="blue"
+        ):
+            # create a primer for application of window without having to
+            # modify x but instead the filter itself
+            cc_primer = np.zeros(x.shape[1:])
+            # now we have a sparse vectore that can be used to convolve
+            cc_primer[self.windows[i]] = self.weights
+            t = cc_primer * x
+            t = t + (self.bias/(t.size/len(t)))  # commute addition before sum
+            t = self.activation_function.forward(t)
+            activated.append(t)
+        return np.array(activated)
 
-    def update(self):
-        """Update node state/ weights for a single example."""
-        self.updater(parm_names=[], it=1)
+    @Layer.bwd
+    def backward(self, gradient, x):
+        """Calculate the local gradient of this CNN.
 
-    def updates(self):
-        """Update node state/ weights for multiple examples simultaneously."""
-        self.updater(parm_names=[])
+        Given the gradient that precedes us,
+        what is the local gradient after us.
+        """
+        ag = gradient
+        x = np.array(x)
+        # calculate gradient with respect to cross correlation
+        # df/dbias is easy as its addition so its same as previous gradient
+        self.bias_gradient = gradient * 1  # uneccessary but here for clarity
+        # gradient from a CNN can be from multiple outputs so need to avg
+        # the batches then sum the gradients that remain
+        self.bias_gradient = np.sum(np.sum(self.bias_gradient, axis=1) /
+                                    self.bias_gradient.shape[1], axis=0)
+        # for each window slice apply window to cached x to find what weights
+        # were multiplied against
+        per_batch_windows = []
+        for i in tqdm(range(len(self.windows)), desc="{}.{}".format(
+                self.__class__.__name__, "backward-window"),
+            ncols=80, colour="blue"
+        ):
+            batch_window = np.array(
+                list(map(lambda a: a[self.windows[i]], x)))
+            per_batch_windows.append(batch_window)
+        windows = np.array(per_batch_windows)
 
-    @property
-    def weights(self):
-        """Get cross convolved filter n-dimensional weights or error."""
-        if self.__dict__.get("_w") is None:
-            raise ValueError("No weight values have been given to this CNN.")
-        return self._w
+        # calculate weight gradient by expanding the gradient dims to match
+        # the window dims so they can be broadcast then sum them allong the
+        # number of filters axis
+        t = gradient
+        len_diff = len(windows.shape) - len(gradient.shape)
+        # expand the dimensions of the ndarray according to the difference
+        for i in range(len_diff):
+            t = np.expand_dims(t, axis=t.ndim)
+        self.weights_gradients = (windows * t).sum(axis=0).mean(axis=0)
 
-    @weights.setter
-    def weights(self, weights):
-        """Set cross convolved filter n-dimensional weights."""
-        # initialise weights from tuple dimensions
-        # TODO: properly implement xavier weight initialisation over np.rand
-        if isinstance(weights, tuple):
-            # https://www.coursera.org/specializations/deep-learning
-            # https://towardsdatascience.com/weight-initialization-techniques-in-neural-networks-26c649eb3b78
-            self._w = np.random.rand(*weights)
-            # ensure initial product of weights * x is in range 0-1
-            # since each product of these wieghts is summed lets ensure they
-            # are smaller than 1 when they are summed by dividing the weights
-            # (not X as its a cyphertext) by the number of elements being sumd
-            self._w = self.weights / self.weights.size
-        else:
-            self._w = weights
-
-    @property
-    def bias(self):
-        """Get sum of products bias coefficient."""
-        if self.__dict__.get("_b") is not None:
-            return self._b
-        else:
-            self.bias = np.array([0])
-            return self.bias
-
-    @bias.setter
-    def bias(self, bias: np.ndarray):
-        """Set sum of products bias coefficient."""
-        self._b = bias
-
-    @property
-    def stride(self):
-        """Get stride over convolutions."""
-        if self.__dict__.get("_stride") is not None:
-            return self._stride
-        else:
-            self.stride = 1
-            return self.stride
-
-    @stride.setter
-    def stride(self, stride):
-        """Set stride over convolutions."""
-        self._stride = stride
+        # dont care as end of computational chain for now
+        # TODO finish calculating gradient of inputs with respect to cc outputs
+        # return local gradient
+        df_dx = np.random.rand(len(x))
+        return df_dx
 
     @property
     def windows(self):
-        """Get current list of windows for cross correlation."""
         if self.cache.get("windows") is not None:
             return self.cache["windows"]
-        return None
 
     @windows.setter
     def windows(self, windows):
-        """Set current list of windows into the data."""
         self.cache["windows"] = windows
-
-    # UTILITY
 
     def windex(self, data: list, filter: list, stride: list,
                dimension: int = 0, partial: list = []):
@@ -219,19 +205,3 @@ class CNN(Node):
             t = (slice(dimension[0], dimension[-1]+1),)
             slicedex += t
         return slicedex
-
-    def probe_shape(self, lst: list):
-        """Get the shape of a list, assuming each sublist is the same length.
-
-        This function is recursive, sending the sublists down and terminating
-        once a type error is thrown by the final point being a non-list
-        """
-        if isinstance(lst, list):
-            # try appending current length with recurse of sublist
-            try:
-                return (len(lst),) + self.probe_shape(lst[0])
-            # once we bottom out and get some non-list type abort and pull up
-            except (AttributeError, IndexError):
-                return (len(lst),)
-        else:
-            return lst.shape
