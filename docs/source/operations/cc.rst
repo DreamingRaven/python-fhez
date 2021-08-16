56543272d7743a6edee3cc7ede8d87b02039ab43
GeorgeRaven <GeorgeRavenCommunity@pm.me>
2021-08-16 16:53:03 +0100

    Added new operations to docs and referenced

---
+++ docs/source/operations/cc.rst
@@ -0,0 +1,12 @@
+.. include:: /substitutions
+
+.. _section_cc:
+
+Cross Correlation (CC)
+######################
+
+
+Our implementation API:
+
+.. automodule:: fhez.nn.operations.cc
+  :members:
---
+++ fhez/nn/layer/cnn.py
@@ -0,0 +1,8 @@
+"""CNN has been split into its constituent operations.
+
+See fhez.nn.operations.cnn -> |section_cc|
+"""
+# @Author: George Onoufriou <archer>
+# @Date:   2021-08-16T16:43:29+01:00
+# @Last modified by:   archer
+# @Last modified time: 2021-08-16T16:51:31+01:00
