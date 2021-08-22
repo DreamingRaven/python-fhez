751a7a2ff6affd1b010ee75a4d8ac3be4005a2ac
GeorgeRaven <GeorgeRavenCommunity@pm.me>
2021-08-22 17:54:09 +0100

    Added numpyfield rough implementation
    For now I will ignore this serialisation step. I will come back to this
    a little later once the core functionality is finished.

--- fhez/fields/__init__.py
+++ fhez/fields/__init__.py
@@ -1,5 +1,19 @@
 # @Author: GeorgeRaven <archer>
 # @Date:   2021-02-11T15:10:37+00:00
 # @Last modified by:   archer
-# @Last modified time: 2021-08-20T15:48:29+01:00
+# @Last modified time: 2021-08-22T16:44:40+01:00
 # @License: please see LICENSE file in project root
+
+# import os
+# import glob
+#
+# __init__path = os.path.abspath(__file__)
+# print("fields_path", __init__path)
+# # get file paths for all py files/ modules in this dir
+# modules = glob.glob("{}*.py".format(os.path.abspath(__file__)))
+#
+# # get filr paths for all tests in this dir as we dont want to import them
+# tests = glob.glob("*_test.py")
+#
+# print("modules", modules)
+# print("tests", tests)
