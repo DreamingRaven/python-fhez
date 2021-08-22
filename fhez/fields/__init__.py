5d0c1f69276d3b2f83f2c3996b00f9dc98e80f05
GeorgeRaven <GeorgeRavenCommunity@pm.me>
2021-08-22 16:18:50 +0100

    Minor changes to __init__ and testing + running
    No major changes just some formatting and doctstrings as well as
    outlining numpy field class but not populated yet.

--- fhez/fields/__init__.py
+++ fhez/fields/__init__.py
@@ -1,6 +1,5 @@
-"""Python-FHEz Fully Homomorphically Encrypted (FHE) Deep Learning Library."""
 # @Author: GeorgeRaven <archer>
 # @Date:   2021-02-11T15:10:37+00:00
 # @Last modified by:   archer
-# @Last modified time: 2021-08-22T15:58:17+01:00
+# @Last modified time: 2021-08-20T15:48:29+01:00
 # @License: please see LICENSE file in project root
