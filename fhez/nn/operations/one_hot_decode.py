790867924cf63e6bc26266aa7ce592629f488063
GeorgeRaven <GeorgeRavenCommunity@pm.me>
2021-08-23 12:17:01 +0100

    Added one hot decoder implementation and tests
    All seems to be working with one hot decoder, this was the last piece
    missing for pure prediction so now we can train and predict. I just need
    to work on the network traversal and boom we are almost ready for
    version 1

--- fhez/nn/operations/one_hot_decode.py
+++ fhez/nn/operations/one_hot_decode.py
@@ -1,12 +1,44 @@
 # @Author: George Onoufriou <archer>
 # @Date:   2021-08-23T10:27:46+01:00
 # @Last modified by:   archer
-# @Last modified time: 2021-08-23T10:29:21+01:00
+# @Last modified time: 2021-08-23T12:08:31+01:00
 
-from collections import deque
 import numpy as np
+import marshmallow as mar
+from fhez.nn.graph.serialise import Serialise
 from fhez.nn.graph.node import Node
 
 
-class OneHotDecode(Node):
-    pass
+class OneHotDecode(Node, Serialise):
+    """Encode value in one-hot encoded sparse array."""
+
+    @property
+    def schema(self):
+        """Get marshmallow serialisation schema."""
+        schema_dict = {
+        }
+        return mar.Schema.from_dict(schema_dict)
+
+    @property
+    def cost(self):
+        """Get non-cost of this node."""
+        return 0
+
+    def forward(self, x: np.ndarray):
+        """Encode input to sparse matrix."""
+        self.inputs.append(x)
+        clss = x.argmax()
+        return clss
+
+    def backward(self, gradient: np.ndarray):
+        """Map only the gradient of the encoded backward."""
+        x = self.inputs.pop()
+        return x * gradient
+
+    def update(self):
+        """Do nothing as nothing to update."""
+        return NotImplemented
+
+    def updates(self):
+        """Do nothing as nothing to update."""
+        return NotImplemented
--- fhez/nn/operations/one_hot_decode_test.py
+++ fhez/nn/operations/one_hot_decode_test.py
@@ -1,4 +1,71 @@
 # @Author: George Onoufriou <archer>
 # @Date:   2021-08-23T10:27:46+01:00
 # @Last modified by:   archer
-# @Last modified time: 2021-08-23T10:27:47+01:00
+# @Last modified time: 2021-08-23T12:16:16+01:00
+
+import time
+
+import unittest
+import numpy as np
+
+from fhez.nn.operations.one_hot_decode import OneHotDecode
+
+
+class OneHotDecodeTest(unittest.TestCase):
+    """Test one hot decoder node."""
+
+    @property
+    def data_shape(self):
+        """Define desired data shape."""
+        return (3, 32, 32, 3)
+
+    @property
+    def data(self):
+        """Get some generated data."""
+        array = np.random.rand(*self.data_shape)
+        return array
+
+    @property
+    def reseal_args(self):
+        """Get some reseal arguments for encryption."""
+        return {
+            "scheme": 2,  # seal.scheme_type.CKK,
+            "poly_modulus_degree": 8192*2,  # 438
+            # "coefficient_modulus": [60, 40, 40, 60],
+            "coefficient_modulus":
+                [45, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 45],
+            "scale": pow(2.0, 30),
+            "cache": True,
+        }
+
+    def setUp(self):
+        """Start timer and init variables."""
+        self.startTime = time.time()
+
+    def tearDown(self):
+        """Calculate and print time delta."""
+        t = time.time() - self.startTime
+        print('%s: %.3f' % (self.id(), t))
+
+    def test_init(self):
+        """Check object is initialised properly."""
+        decoder = OneHotDecode()
+        self.assertIsInstance(decoder, OneHotDecode)
+
+    def test_forward(self):
+        """Check decoding forward pass working as expected."""
+        decode = OneHotDecode()
+        one_hot = np.eye(5)[3]
+        not_hot = decode.forward(one_hot)
+        self.assertEqual(not_hot, np.argmax(one_hot))
+
+    def test_backward(self):
+        """Check only one hot gradient is chained."""
+        decode = OneHotDecode()
+        one_hot = np.eye(5)[3]
+        not_hot = decode.forward(one_hot)
+        self.assertEqual(not_hot, np.argmax(one_hot))
+        local_grad = decode.backward(2)
+        np.testing.assert_array_almost_equal(local_grad, one_hot * 2,
+                                             decimal=1,
+                                             verbose=True)
