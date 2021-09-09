46997b6ef8e9d4a851aee0c57f9739db12ebd3d6
GeorgeRaven <GeorgeRavenCommunity@pm.me>
2021-09-09 15:44:39 +0100

    Added serialisation fields checks and dumps

--- fhez/fields/numpyfield_test.py
+++ fhez/fields/numpyfield_test.py
@@ -1,7 +1,7 @@
 # @Author: George Onoufriou <archer>
 # @Date:   2021-08-22T16:32:08+01:00
 # @Last modified by:   archer
-# @Last modified time: 2021-08-22T17:53:44+01:00
+# @Last modified time: 2021-09-09T15:42:30+01:00
 
 import time
 import unittest
@@ -35,7 +35,7 @@ class NumpyFieldTest(unittest.TestCase):
         t = time.time() - self.startTime
         print('%s: %.3f' % (self.id(), t))
 
-    def testSerialDeserial(self):
+    def test_serialDeserialFloat(self):
         """Check serialisation and deserialisation working properly."""
         # set up necessary vars
         data = self.data
@@ -47,3 +47,72 @@ class NumpyFieldTest(unittest.TestCase):
 
         # now test deserialisation
         deserial = schema().load(out)
+
+        # print("DESERIAL OUTPUT TYPE: {}".format(type(out)))
+        self.assertIsInstance(out, dict)
+
+        # check equality
+        np.testing.assert_array_almost_equal(
+            sample["data"],
+            deserial["data"],
+            decimal=1,
+            verbose=True)
+
+        # check type is as original/ expected
+        # self.assertTrue(np.issubdtype(sample["data"].dtype, np.floating))
+        self.assertEqual(sample["data"].dtype, sample["data"].dtype)
+
+    def test_serialDeserialsFloat(self):
+        """Check serialisation and deserialisation working properly."""
+        # set up necessary vars
+        data = self.data
+        sample = {"data": data}
+        schema = mar.Schema.from_dict({"data": NumpyField()})
+
+        # now test serialisation
+        out = schema().dumps(sample)
+
+        # print("DESERIALS OUTPUT TYPE: {}".format(type(out)))
+        self.assertIsInstance(out, str)
+
+        # now test deserialisation
+        deserial = schema().loads(out)
+
+        # check equality
+        np.testing.assert_array_almost_equal(
+            sample["data"],
+            deserial["data"],
+            decimal=1,
+            verbose=True)
+
+        # check type is as original/ expected
+        # self.assertTrue(np.issubdtype(sample["data"].dtype, np.floating))
+        self.assertEqual(sample["data"].dtype, sample["data"].dtype)
+
+    def test_serialDeserialInt(self):
+        """Check serialisation and deserialisation working properly."""
+        # set up necessary vars
+        data = self.data.astype(int)
+        # all of the data in the sample will inevitably be 0 since trunication
+        sample = {"data": data}
+        schema = mar.Schema.from_dict({"data": NumpyField()})
+
+        # now test serialisation
+        out = schema().dump(sample)
+
+        # print("DESERIAL OUTPUT TYPE: {}".format(type(out)))
+        self.assertIsInstance(out, dict)
+
+        # now test deserialisation
+        deserial = schema().load(out)
+
+        # check equality
+        np.testing.assert_array_almost_equal(
+            sample["data"],
+            deserial["data"],
+            decimal=1,
+            verbose=True)
+
+        # check type is as original/ expected
+        # self.assertTrue(np.issubdtype(sample["data"].dtype, np.integer))
+        self.assertEqual(sample["data"].dtype, sample["data"].dtype)
