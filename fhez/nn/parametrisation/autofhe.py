ca80d5dfc6d4087da3e9ef69ea4637c96d90842a
GeorgeRaven <GeorgeRavenCommunity@pm.me>
2021-09-16 15:08:43 +0100

    Added concern for decrypt

--- fhez/nn/parametrisation/autofhe.py
+++ fhez/nn/parametrisation/autofhe.py
@@ -2,10 +2,11 @@
 # @Author: George Onoufriou <archer>
 # @Date:   2021-09-14T10:34:17+01:00
 # @Last modified by:   archer
-# @Last modified time: 2021-09-16T14:47:52+01:00
+# @Last modified time: 2021-09-16T15:07:29+01:00
 
 from fhez.nn.graph.utils import assign_edge_costing
 from fhez.nn.operations.encrypt import Encrypt
+from fhez.nn.operations.decrypt import Decrypt
 
 
 def autoHE(graph, node: str, cost: int = None,
@@ -31,7 +32,7 @@ def autoHE(graph, node: str, cost: int = None,
     """
     cost = cost if cost is not None else 0
     cost_edges = cost_edges if cost_edges is not None else True
-    concern = tuple(concern if concern is not None else [Encrypt])
+    concern = tuple(concern if concern is not None else [Encrypt, Decrypt])
     assert graph is not None, "graph should exist, cannot operate on none!"
 
     # ensures all edges are labeled with weigts that correspond to
