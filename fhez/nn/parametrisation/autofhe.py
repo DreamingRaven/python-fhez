78a08e3ad751f40d64dc66bc98a85ba3d6fb33ce
GeorgeRaven <GeorgeRavenCommunity@pm.me>
2021-10-14 16:35:11 +0100

    Rought parameterisation still needs tuning
    Params are generated but they seem inefficient values, so I will see if
    I can cut them down and tighten some dials in to make them bareley
    sufficient.

--- fhez/nn/parametrisation/autofhe.py
+++ fhez/nn/parametrisation/autofhe.py
@@ -2,8 +2,9 @@
 # @Author: George Onoufriou <archer>
 # @Date:   2021-09-14T10:34:17+01:00
 # @Last modified by:   archer
-# @Last modified time: 2021-10-14T14:47:49+01:00
+# @Last modified time: 2021-10-14T16:22:42+01:00
 
+import numpy as np
 from fhez.nn.graph.utils import assign_edge_costing
 from fhez.nn.operations.encrypt import Encrypt
 from fhez.nn.operations.decrypt import Decrypt
@@ -15,6 +16,13 @@ def source_prop(graph,
                 node: str,
                 concern: list = None,
                 cost: int = None):
+    """Propagate some source through the network.
+
+    .. note::
+
+        You are porbably not interested in this function, it is here for manual
+        control and as a subpart of autoHE and in future autoFHE.
+    """
     cost = cost if cost is not None else 0
     concern = tuple(concern if concern is not None else [Encrypt,
                                                          Decrypt,
@@ -32,7 +40,7 @@ def source_prop(graph,
         elif node_data["sources"].get(source) < cost:
             node_data["sources"][source] = cost
 
-    print(node, node_data["sources"])
+    # print(node, node_data["sources"])
 
     # on concerned nodes which is not our initial one
     if isinstance(node_object, concern) and source != node:
@@ -105,5 +113,57 @@ def autoHE(graph, nodes, concern=None, cost_edges=None):
                     # increase group cost if greater than ours
                     if src[key] > groups[1][groups[0][i]]:
                         groups[1][groups[0][i]] = src[key]
-    print("GROUPS", groups)
+    # print("GROUPS", groups)
     return groups
+
+
+def temp_encryptor_generator(cost, scale_pow=30, special_mult=1.5):
+    """Given some cost generate encryption parameters.
+
+    .. note::
+
+        This is highly subject to removal, this is here in the interip untill a
+        full specification for Erray can be finalised.
+
+    +---------------------+------------------------------+
+    | poly_modulus_degree | max coeff_modulus bit-length |
+    +=====================+==============================+
+    | 1024                | 27                           |
+    +---------------------+------------------------------+
+    | 2048                | 54                           |
+    +---------------------+------------------------------+
+    | 4096                | 109                          |
+    +---------------------+------------------------------+
+    | 8192                | 218                          |
+    +---------------------+------------------------------+
+    | 16384               | 438                          |
+    +---------------------+------------------------------+
+    | 32768               | 881                          |
+    +---------------------+------------------------------+
+
+    slots (CKKS) = poly_modulus_degree/2
+    """
+    dummy = {
+        "scheme": 2,  # seal.scheme_type.CKK,
+        "poly_modulus_degree": 8192*2,  # 438
+        # "coefficient_modulus": [60, 40, 40, 60],
+        "coefficient_modulus":
+        [45, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 45],
+        "scale": pow(2.0, 30),
+        "cache": True,
+    }
+    coef_mod = np.ones((cost+2,)) * scale_pow
+    coef_mod[0] *= special_mult
+    coef_mod[-1] *= special_mult
+    max_coef_mod_bits = 27
+    while max_coef_mod_bits < np.sum(coef_mod):
+        max_coef_mod_bits *= 2
+
+    parms = {
+        "scheme": 2,
+        "poly_modulus_degree": int(1024 * (max_coef_mod_bits/27)),
+        "coefficient_modulus": coef_mod.astype(int).tolist(),
+        "scale": pow(2.0, scale_pow),
+        "cache": True,
+    }
+    return parms
--- fhez/nn/parametrisation/autofhe_test.py
+++ fhez/nn/parametrisation/autofhe_test.py
@@ -1,14 +1,15 @@
 # @Author: George Onoufriou <archer>
 # @Date:   2021-09-14T11:51:45+01:00
 # @Last modified by:   archer
-# @Last modified time: 2021-10-14T13:51:02+01:00
+# @Last modified time: 2021-10-14T16:26:57+01:00
 
 import time
 import unittest
 import numpy as np
 from fhez.nn.graph.prefab import cnn_classifier, basic
-from fhez.nn.parametrisation.autofhe import autoHE
+from fhez.nn.parametrisation.autofhe import autoHE, temp_encryptor_generator
 from fhez.nn.traverse.firing import Firing
+from fhez.rearray import ReArray
 
 
 class AutoHE(unittest.TestCase):
@@ -56,7 +57,10 @@ class AutoHE(unittest.TestCase):
         """Check autohe can auto parametrise as expected."""
         graph = self.graph
         # auto parametrise all encrypted input nodes and their paths
-        autoHE(graph=graph, nodes=["x_0", "x_1", "y_0"])
+        groups = autoHE(graph=graph, nodes=["x_0", "x_1", "y_0"])
+        np.testing.assert_array_almost_equal(groups[1], [4, 4],
+                                             decimal=4,
+                                             verbose=True)
         nf = Firing(graph)
         nf.stimulate(neurons=np.array([]), signals=np.array([]),
                      receptor="forward")
@@ -69,6 +73,21 @@ class AutoHE(unittest.TestCase):
     #     nf = Firing(graph)
     #     nf.stimulate(neurons=np.array([]), signals=np.array([]),
     #                  receptor="forward")
+
+    def test_temp_encryptor_generator(self):
+        cost = 6
+        parms = temp_encryptor_generator(cost=cost)
+        print(parms)
+        # x = self.data.flatten()[:10]
+        x = np.array([0.5, 0.3, 0.7, 0.9, 0.1])
+        cyphertxt = ReArray(x, **parms)
+        for _ in range(cost):
+            cyphertxt = np.multiply(cyphertxt, 0.5)
+            print("RUN")
+        print(x)
+        np.testing.assert_array_almost_equal(cyphertxt, x/(cost*2),
+                                             decimal=6,
+                                             verbose=True)
 
     def test_todo(self):
         """Todo note to fail tests so it cant be forgotten."""
